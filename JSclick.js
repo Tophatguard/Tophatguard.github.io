0753b8729c935844346660e3246ed9754182cff9
Tophatguard <144190236+Tophatguard@users.noreply.github.com>
2024-05-11 16:40:03 -0500

    fix

--- JSclick.js
+++ JSclick.js
@@ -2,5 +2,5 @@ var points = 0;
 
 function getPoint() {
     points += 1;
-    document.getElementById('JSicon').innerText = 'your points: ' + points;
+    document.getElementById('pointsT').innerText = 'your points: ' + points;
 }
